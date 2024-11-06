25c8f9edf34852bbcbc81d29f04799ed05e937df
Jacco van Ossenbruggen <Jacco.van.Ossenbruggen@vu.nl>
2024-11-06 17:45:28 +0100

    Only write to production dataset when really in production mode, all else go to acceptance

--- src/cbs_codelib.ts
+++ src/cbs_codelib.ts
@@ -29,11 +29,9 @@ const destination = {
   account: process.env.USER ?? "odissei",
   prefixes: prefix,
   dataset:
-    Etl.environment === environments.Acceptance
-      ? "odissei-acceptance"
-      : Etl.environment === environments.Testing
-        ? "odissei-acceptance"
-        : "odissei",
+    Etl.environment === environments.Production
+      ? "odissei"
+      : "odissei-acceptance",
 };
 
 export default async function (): Promise<Etl> {
