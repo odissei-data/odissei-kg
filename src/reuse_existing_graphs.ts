208f3d42cebe7ad3d79e47d743f9b6e25449b5f6
Jacco van Ossenbruggen <Jacco.van.Ossenbruggen@vu.nl>
2024-11-22 12:16:48 +0100

    code cleanup

--- src/reuse_existing_graphs.ts
+++ src/reuse_existing_graphs.ts
@@ -5,14 +5,12 @@ const jobs = [ {source_location: 'https://www.dublincore.org/specifications/bibo
 
 export default async function (): Promise<Etl> {
   const etl = new Etl(destination);
-  const account = destination.account;
-  const dataset = destination.dataset;
   for (const j of jobs) { 
     await etl.copySource(
         Source.url(j.source_location),
         Destination.triplyDb({
-          account: account,
-          dataset: dataset,
+          account: destination.account,
+          dataset: destination.dataset,
           opts: { defaultGraph: j.destination_graph, synchronizeServices: false }
         })
     )}
