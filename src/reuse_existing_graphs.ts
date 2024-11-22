278ab5d6b87489e1f1693f5e8131c8954e5d35d6
Jacco van Ossenbruggen <Jacco.van.Ossenbruggen@vu.nl>
2024-11-22 18:55:49 +0100

    Added portal knowledge graph from fuseki

--- src/reuse_existing_graphs.ts
+++ src/reuse_existing_graphs.ts
@@ -2,14 +2,17 @@ import { Etl, Source, Destination } from "@triplyetl/etl/generic";
 import { destination } from "./utils/odissei_kg_utils.js";
 
 const jobs = [ 
-  { source_location: 'https://www.dublincore.org/specifications/bibo/bibo/bibo.ttl', 
+  { source_location:   'https://www.dublincore.org/specifications/bibo/bibo/bibo.ttl', 
     destination_graph: 'https://www.dublincore.org/specifications/bibo/'
   },
-  { source_location: 'https://vocabs.ardc.edu.au/registry/api/resource/downloads/4948/codata_codata-research-data-management-terminology_v001.ttl',
+  { source_location:   'https://vocabs.ardc.edu.au/registry/api/resource/downloads/4948/codata_codata-research-data-management-terminology_v001.ttl',
     destination_graph: 'https://terms.codata.org/rdmt/'
   },
-  { source_location: 'https://www.w3.org/TR/skos-reference/skos.rdf',
+  { source_location:   'https://www.w3.org/TR/skos-reference/skos.rdf',
     destination_graph: 'https://www.w3.org/TR/skos-reference/skos.rdf'
+  },
+  { source_location:   'https://fuseki.devstack.odissei.nl/odissei/',
+    destination_graph: 'https://fuseki.devstack.odissei.nl/odissei/'
   }
   ]
 
