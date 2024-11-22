48f60b1ddd8ca63a7990fa7f475f7c8c4e648675
Jacco van Ossenbruggen <Jacco.van.Ossenbruggen@vu.nl>
2024-11-22 11:14:23 +0100

    Fixed typo, upload script now working

--- upload_existing_graphs.sh
+++ upload_existing_graphs.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/env bash
 
+# To run this script, please clone https://github.com/odissei-data/vocabularies
+# in the same parent folder as this repo.
+VOCAB_FOLDER=../vocabularies
+
 # Get your TRIPLYDB_TOKEN from your .env file (please do not commit)
-USER=odissei
 source .env
 
-DATASET="odissi-kg-acceptance" && [[ "$ENV" == "Production" ]] && DATASET="odissei-kg"
+DATASET="odissei-kg-acceptance" && [[ "$ENV" == "Production" ]] && DATASET="odissei-kg"
 API_URI=https://api.kg.odissei.nl/datasets/$USER/$DATASET/jobs
 
-SKOS_FILE=./data/test/skos.rdf 
+SKOS_FILE=$VOCAB_FOLDER/w3c/skos.rdf
 
 echo "Uploading to $API_URI"
 curl -H "Authorization: Bearer $TRIPLYDB_TOKEN" $API_URI -F file=@$SKOS_FILE
