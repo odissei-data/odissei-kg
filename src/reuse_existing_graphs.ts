2125be51bda3bf9858b57c526d05cb36f92dd12d
Jacco van Ossenbruggen <Jacco.van.Ossenbruggen@vu.nl>
2024-12-04 09:56:43 +0100

    Added uploading of cbs-variables-thesaurus.ttl asset into named graph https://w3id.org/odissei/cv/cbs/variableThesaurus/

--- src/reuse_existing_graphs.ts
+++ src/reuse_existing_graphs.ts
@@ -2,25 +2,32 @@ import { Etl, Source, Destination } from "@triplyetl/etl/generic";
 import { destination } from "./utils/odissei_kg_utils.js";
 
 const jobs = [ 
-  { source_location:   'https://www.dublincore.org/specifications/bibo/bibo/bibo.ttl', 
+  { source_location:   Source.url('https://www.dublincore.org/specifications/bibo/bibo/bibo.ttl'), 
     destination_graph: 'https://www.dublincore.org/specifications/bibo/'
   },
-  { source_location:   'https://vocabs.ardc.edu.au/registry/api/resource/downloads/4948/codata_codata-research-data-management-terminology_v001.ttl',
+  { source_location:   Source.url('https://vocabs.ardc.edu.au/registry/api/resource/downloads/4948/codata_codata-research-data-management-terminology_v001.ttl'),
     destination_graph: 'https://terms.codata.org/rdmt/'
   },
-  { source_location:   'https://www.w3.org/TR/skos-reference/skos.rdf',
+  { source_location:   Source.url('https://www.w3.org/TR/skos-reference/skos.rdf'),
     destination_graph: 'https://www.w3.org/TR/skos-reference/skos.rdf'
   },
-  { source_location:   'https://fuseki.devstack.odissei.nl/odissei/',
+  { source_location: Source.TriplyDb.asset(destination.account, destination.dataset, {name: 'cbs-variables-thesaurus.ttl'}),
+    destination_graph: 'https://w3id.org/odissei/cv/cbs/variableThesaurus/'
+  },
+  { source_location:   Source.url('https://fuseki.devstack.odissei.nl/odissei/'),
     destination_graph: 'https://fuseki.devstack.odissei.nl/odissei/'
   }
   ]
+
+//  Source.TriplyDb.asset(destination.account, destination.dataset, {
+//   name: "projecten_met_bestanden_einddatum_na_2023.xlsx",
+//  }),
 
 export default async function (): Promise<Etl> {
   const etl = new Etl(destination);
   for (const j of jobs) { 
     await etl.copySource(
-        Source.url(j.source_location),
+        j.source_location,
         Destination.triplyDb({
           account: destination.account,
           dataset: destination.dataset,
