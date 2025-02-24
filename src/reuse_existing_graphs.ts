bc1220e38f4c2247681abfd502e94cc351bf82e8
Jacco van Ossenbruggen <Jacco.van.Ossenbruggen@vu.nl>
2025-02-24 12:05:00 +0100

    clean up spacing warnings by gitdiff

--- src/reuse_existing_graphs.ts
+++ src/reuse_existing_graphs.ts
@@ -24,7 +24,7 @@ const jobs = [
     destination_graph:  'https://tools.clariah.nl/data/'
   },
   { source_location: Source.url('https://thesauri.cessda.eu/rest/v1/elsst-4/data?format=text/turtle'),
-    destination_graph: 'urn:ddi:int.cessda.elsst:00000000-0000-0000-0000-000000000001:4' 
+    destination_graph: 'urn:ddi:int.cessda.elsst:00000000-0000-0000-0000-000000000001:4'
   },
   { source_location: Source.TriplyDb.asset(destination.account, destination.dataset, {name: 'cbs-variables-thesaurus.ttl'}),
     destination_graph: 'https://w3id.org/odissei/cv/cbs/variableThesaurus/'
