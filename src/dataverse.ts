800c7b651c9ec9e0b2c1b90ca05f1e5c73c3d1e1
Jacco van Ossenbruggen <Jacco.van.Ossenbruggen@vu.nl>
2025-04-04 13:56:16 +0200

    rename dv graph from dataverse_vu_stagging to dataverse

--- src/dataverse.ts
+++ src/dataverse.ts
@@ -1,14 +1,13 @@
 import { Etl, loadRdf, Source, toTriplyDb, _switch } from '@triplyetl/etl/generic'
 
 import { destination, prefix } from "./utils/odissei_kg_utils.js";
-import etlDataset from './utils/etlDataset.js';
 import fromDataVerse from './utils/fromDataVerse.js'
-import etlDataverse from './utils/etlDataverse.js'
+import etlDataverse  from './utils/etlDataverse.js'
+import etlDataset    from './utils/etlDataset.js';
 
 
 var my_destination: any = destination;
-//my_destination.defaultGraph = prefix.graph.concat('dataverse_vu');
-my_destination.defaultGraph = prefix.graph.concat('dataverse_vu_stagging');
+my_destination.defaultGraph = prefix.graph.concat('dataverse');
 
 export default async function (): Promise<Etl> {
   const etl = new Etl(my_destination)
