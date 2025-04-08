c3d108b85dc301ef392551aac5e908427018a3f0
“Andre <firmao@gmail.com>
2025-04-08 16:37:03 +0200

    use static test file for the time being

--- src/dataverse.ts
+++ src/dataverse.ts
@@ -1,9 +1,12 @@
-import { Etl, loadRdf, Source, toTriplyDb, _switch } from '@triplyetl/etl/generic'
+import { Etl, loadRdf, Source, toTriplyDb, _switch, when, fromJson } from '@triplyetl/etl/generic'
+import { iri, iris, objects, split, str } from '@triplyetl/etl/ratt'
+
 
 import { destination, prefix } from "./utils/odissei_kg_utils.js";
 import fromDataVerse from './utils/fromDataVerse.js'
 import etlDataverse  from './utils/etlDataverse.js'
 import etlDataset    from './utils/etlDataset.js';
+import { logRecord } from '@triplyetl/etl/debug';
 
 
 var my_destination: any = destination;
@@ -12,12 +15,16 @@ my_destination.defaultGraph = prefix.graph.concat('dataverse-staging');
 export default async function (): Promise<Etl> {
   const etl = new Etl(my_destination)
   etl.use(
-    fromDataVerse(),
+    //fromDataVerse(),
+    fromJson(Source.file('./static/dataverseTest.json')),
+    etlDataset(),
+    //logRecord(),
     // always run below:
-    _switch('type',
-      ['dataverse', etlDataverse()],
-      ['dataset', etlDataset()]
-    ),
+    // _switch('type',
+    //   ['dataverse', etlDataverse()],
+    //   ['dataset', etlDataset()]
+    // ),
+
     toTriplyDb(my_destination)
   )
   return etl
