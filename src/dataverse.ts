18e8e160c20b6b7eac8fcc01f5f9cb92fe5d389e
Jacco van Ossenbruggen <Jacco.van.Ossenbruggen@vu.nl>
2025-04-11 14:51:02 +0200

    Cleanup main dataverseETL

--- src/dataverse.ts
+++ src/dataverse.ts
@@ -1,13 +1,8 @@
-import { Etl, loadRdf, Source, toTriplyDb, _switch, when, fromJson } from '@triplyetl/etl/generic'
-import { iri, iris, objects, split, str } from '@triplyetl/etl/ratt'
-
-
+import { Etl, toTriplyDb, _switch } from '@triplyetl/etl/generic'
 import { destination, prefix } from "./utils/odissei_kg_utils.js";
 import fromDataVerse from './utils/fromDataVerse.js'
 import etlDataverse  from './utils/etlDataverse.js'
 import etlDataset    from './utils/etlDataset.js';
-import { logRecord } from '@triplyetl/etl/debug';
-
 
 var my_destination: any = destination;
 my_destination.defaultGraph = prefix.graph.concat('dataverse-staging');
@@ -18,15 +13,10 @@ export default async function (): Promise<Etl> {
   const etl = new Etl(my_destination);
   etl.use(
     fromDataVerse(),
-    //fromJson(Source.file('./static/dataverseTest.json')),
-    //etlDataset(),
-    //logRecord(),
-    // always run below:
      _switch('type',
        ['dataverse', etlDataverse()],
        ['dataset', etlDataset()]
      ),
-
     toTriplyDb(my_destination)
   )
   return etl
