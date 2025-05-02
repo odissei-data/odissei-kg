d229a8b6c86c18da96cbf9a880d53555e9dee491
“Andre <firmao@gmail.com>
2025-05-02 16:16:41 +0200

    add json-ld from dataverse URLs to the graph

--- src/utils/fromDataVerse.ts
+++ src/utils/fromDataVerse.ts
@@ -1,5 +1,6 @@
-import { type Middleware } from '@triplyetl/etl/generic'
-import { prefix } from './odissei_kg_utils.js'
+import { type Middleware, Etl, Source, Destination, loadRdf } from '@triplyetl/etl/generic'
+import { destination, prefix } from './odissei_kg_utils.js'
+import context from '@triplyetl/etl/_internal/ratt/middlewares/transformations/custom/addFrom/context'
 const DataverseApi = prefix.dataverseAPI
 // cons DaverseAPI = ''
 
@@ -11,7 +12,7 @@ const DataverseApi = prefix.dataverseAPI
  *   - Datasets are fetched from eg https://datasets.iisg.amsterdam/api/datasets/9820
  *   - If it's a dataverse reference, we recurse and fetch it's metadata, contents, and all the things it references
  */
-export default function fromApi (): Middleware {
+export default function fromApi (destination: any): Middleware {
   return async function _fromApi (ctx, next) {
     async function handleDataverse (dataverseId: number, parentDataverseId?: number): Promise <void> {
       const [dataverse, dataverseContents] = await fetchDataverse(dataverseId)
@@ -23,10 +24,9 @@ export default function fromApi (): Middleware {
         contents.parentDataverseId = dataverseId
         if (contents.type === 'dataset') {
           try {
-            const dataset = await fetchDataset(contents.protocol, contents.identifier, contents.authority)
-            dataset.type = 'dataset'
-            dataset.dataverseId = dataverseId
-            await next(dataset, ctx.app.getNewStore())
+            const datasetUrl = getDatasetUrl(contents.protocol, contents.identifier, contents.authority)            
+            await ctx.app.copySource(Source.url(datasetUrl), destination)
+            await next();
           } catch (e) {
             console.warn(`Ignoring this error: ${(e as Error).message}`)
             if (!(e as Error).message.startsWith('[500]')) {
@@ -66,18 +66,23 @@ async function fetchDataverse (dataverseId: number): Promise<[any, any]> {
 
   return [dataverse, dataverseContents] as [any, any]
 }
-async function fetchDataset (protocol: string, datasetId: number, authority: string): Promise<any> {
-  const link = `${DataverseApi}/datasets/export?exporter=dataverse_json&persistentId=${protocol}:${authority}/${datasetId}`
-  //const link = '${DataverseApi}/datasets/export?exporter=OAI_ORE&persistentId=${protocol}:${authority}/${datasetId}'
-  const response = await fetch(link)
-  if (response.status !== 200) throw new Error(`[${response.status}] Failed fetching ${link}: ${response.statusText}`)
-  let json: any
-  json = await response.json()
+function getDatasetUrl (protocol: string, datasetId: number, authority: string) {
+  //const link = `${DataverseApi}/datasets/export?exporter=dataverse_json&persistentId=${protocol}:${authority}/${datasetId}`
+  const link = '${DataverseApi}/datasets/export?exporter=OAI_ORE&persistentId=${protocol}:${authority}/${datasetId}'
+  const link2 = DataverseApi + '/datasets/export?exporter=OAI_ORE&persistentId=' + protocol + ':' + authority + '/' + datasetId
+  console.info('Fetching dataset from: ' + link2)
+  return link2
+  // const response = await fetch(link2)
+  // if (response.status !== 200) throw new Error(`[${response.status}] Failed fetching ${link}: ${response.statusText}`)
+  // let json: any
+  // json = await response.json()
   // console.info(json)
-  /**
-   * Remove annoying `data: {}` envelope
-   */
-  json = { ...json, ...json.data }
-  delete json.data
-  return json
+  // json = { ...json, ...json.data }
+  // delete json.data
+  // return json
+
+
+  /*loadRdf(
+    Source.file("./static/dataverseTest.jsonld")
+  )*/
 }
