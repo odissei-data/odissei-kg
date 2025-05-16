3a8ddd6a6eb2d3b93b29e8f7cf406556771cc3b7
“Andre <firmao@gmail.com>
2025-05-16 11:54:43 +0200

    dataverseFix Update query

--- src/dataverseFix.ts
+++ src/dataverseFix.ts
@@ -1,17 +1,14 @@
-import { logQuads } from '@triplyetl/etl/debug'
 import { Etl, loadRdf, Source, toTriplyDb } from '@triplyetl/etl/generic'
-import { construct } from '@triplyetl/etl/sparql'
 import { destination, prefix } from "./utils/odissei_kg_utils.js";
 
 var my_destination: any = destination;
-my_destination.defaultGraph = prefix.graph.concat('schemaFix');
+my_destination.defaultGraph = prefix.dataverse;
 
 export default async function (): Promise<Etl> {
   const etl = new Etl(my_destination)
   etl.use(
     // Load RDF from the follwing query: https://kg.odissei.nl/odissei/-/queries/Fixing-http-replacing-by-https/4
-    loadRdf(Source.TriplyDb.query(destination.account, 'Fixing-http-replacing-by-https')),
-    //loadRdf(Source.TriplyDb.query(destination.account, 'Fixing-replace-literal-String-by-URI')),
+    loadRdf(Source.TriplyDb.query(destination.account, 'delete-insert-dataverse-fix')),
     toTriplyDb(my_destination)
     //logQuads(),
   )
