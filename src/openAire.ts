8b2afbf0ac7cef7916471b63a846fe907764d904
“Andre <firmao@gmail.com>
2025-10-23 18:41:04 +0200

    Fix filter problem from openAire.

--- src/openAire.ts
+++ src/openAire.ts
@@ -9,7 +9,9 @@ import { validate } from '@triplyetl/etl/shacl'
 import { bibo, a, dct, dcm, sdo } from "@triplyetl/etl/vocab"; // dct
 import { destination, prefix } from "./utils/odissei_kg_utils.js";
 
-const open_aire = "https://services.openaire.eu/search/v2/api/reports?format=csv&fq=foslabel%20exact%20%2205%20social%20sciences%7C%7Csocial%20sciences%22&fq=communityId%20exact%20%22netherlands%22&type=publications";
+//const open_aire = "https://services.openaire.eu/search/v2/api/reports?format=csv&fq=foslabel%20exact%20%2205%20social%20sciences%7C%7Csocial%20sciences%22&fq=communityId%20exact%20%22netherlands%22&type=publications";
+//const open_aire = "https://services.openaire.eu/search/v2/api/reports?format=csv&fq=resultacceptanceyear%20within%20%222025%202025%22&fq=foslabel%20exact%20%2205%20social%20sciences%7C%7Csocial%20sciences%22&fq=communityId%20exact%20%22netherlands%22&type=publications";
+const open_aire = "https://tinyurl.com/nada123x";
 
 var my_destination: any = destination;
 my_destination.defaultGraph = prefix.graph.concat("openaire");
