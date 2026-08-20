e45256488ab32e52af66112d6ee7265bf05bd9d4
Jacco van Ossenbruggen <Jacco.van.Ossenbruggen@vu.nl>
2026-08-20 16:32:55 +0200

    cleanup: rm unused code

--- src/dataverse.ts
+++ src/dataverse.ts
@@ -1,8 +1,7 @@
-import { Etl, toTriplyDb, _switch, Source, fromJson, Destination } from '@triplyetl/etl/generic';
+import { Etl, Destination } from '@triplyetl/etl/generic';
 import { destination, prefix } from "./utils/odissei_kg_utils.js";
 import fromDataVerse from './utils/fromDataVerse.js';
 
-//var my_destination = {...destination, defaultGraph: prefix.graph.concat('dataverse-staging')};
 var my_destination = {...destination, defaultGraph: prefix.dataverse};
 
 export default async function (): Promise<Etl> {
@@ -10,17 +9,11 @@ export default async function (): Promise<Etl> {
     prefix.dataverseAPI + " into named graph: " + my_destination.defaultGraph);
   const etl = new Etl(my_destination);
   etl.use(
-    //fromJson(Source.file('./static/dataverseTest.jsonld')),
     fromDataVerse(Destination.triplyDb({
       account: my_destination.account,
       dataset: my_destination.dataset,
       opts: { defaultGraph: my_destination.defaultGraph, synchronizeServices: false }
     })),
-    //  _switch('type',
-    //    ['dataverse', etlDataverse()],
-    //    ['dataset', []]
-    //  ),
-    // toTriplyDb(my_destination)
   )
   return etl
 }
